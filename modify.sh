d09a8cbdc257cb4a74ace61cf776489ff548ea78
Mathew Winters <mathew@winters.org.nz>
2021-01-02 12:38:33 +1300

    first commit

---
+++ modify.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+BRANCH=$1
+
+git clone --depth=1 --single-branch --branch ${BRANCH} https://github.com/Nico640/docker-unms.git
+cd docker-unms || exit 1
+
+VER=$(grep "ubnt/unms:" Dockerfile | cut -f 2 -d ' ' | cut -f 2 -d ':')
+echo "::set-output name={ums_version}::{$VER}"
+
+CRM_UPDATE="psql -U \$UNMS_PG_USER -d \$POSTGRES_DB -c \"update crm_db_version_view set value = '$VER';\""
+rm -rf root/etc/logrotate.d/ucrm || exit 1
+rm -rf root/etc/services.d/ucrm || exit 1
+
+sed -i '/^# UCRM/,+3 s|^|#|' root/etc/cont-init.d/40-prepare || exit 1
+sed -i 's/.*logrotate.d\/ucrm/#&/' root/etc/cont-init.d/40-prepare || exit 1
+
+sed -i "/if.*QUIET_MODE.*then/i $CRM_UPDATE" root/etc/services.d/unms/run || exit 1
+
+sed -i 's/^FROM ubnt\/unms-crm.*/#&/' Dockerfile || exit 1
+sed -i '/^# start unms-crm #$/,/^# end unms-crm #$/ s|^|#|' Dockerfile || exit 1
+sed -i '/^# start php plugins \/ composer #$/,/^# end php plugins \/ composer #$/ s|^|#|' Dockerfile || exit 1
+sed -i 's/^COPY --from=unms-crm.*available-servers.*/#&/' Dockerfile || exit 1
+sed -i '/.*#location \/nms #g" \\/ s/\\//' Dockerfile || exit 1
+sed -i '/.*sed.*\/etc\/nginx\/ucrm\/ucrm.conf.*/,+2 d' Dockerfile || exit 1
+sed -i '/.*#location \/crm #g"/ d' Dockerfile || exit 1
---
+++ readme.md
@@ -0,0 +1,16 @@
+![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/mwinters-stuff/docker-unms-no-crm?sort=semver)
+![Create release with docker](https://github.com/mwinters-stuff/docker-unms-no-crm/workflows/Create%20release%20with%20docker/badge.svg)
+# Docker image modification of https://github.com/Nico640/docker-unms without UCRM
+
+Usage is the same as for https://github.com/Nico640/docker-unms but without UCRM
+
+* Builds are for arm64 and amd64
+
+## Usage
+Initially start with the original version (Nico640/docker-unms), this will create the ucrm database, make sure you are all
+running correctly then use the following image.
+
+ghcr.io/mwinters-stuff/docker-unms-no-crm:latest
+
+I will attempt to keep up-to-date with the latest from Nico640/docker-unms
+
